5678479eae6febf41c9e7a4707d90a85ca799db5
wxqianggo <wxqiang@gmail.com>
2023-05-17 22:14:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import streamlit as st
 from datetime import time, datetime
 
 st.header('st.slider')
+
+# 样例 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
