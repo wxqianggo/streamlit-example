dd814db95cd2b09be17e3970df09dcc559f62fbd
wxqianggo <wxqiang@gmail.com>
2023-05-24 14:19:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
+import openai
 import streamlit as st
+
+openai.api_key = "YOUR_API_KEY"
+
+# Define the function to generate responses using GPT-3
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = response.choices[0].text
+    return message.strip()
+
 
 # Homepage
 def homepage():
-    st.title("Welcome to our new startup!")
-    st.write("We specialize in talking with specialists by GPT.")
-    st.write("Check out our About and Contact pages to learn more!")
+    st.markdown(
+        """
+        <div style='background-color: #f5f5f5; padding: 5rem 0;'>
+            <h1 style='text-align: center; font-size: 4rem; margin-bottom: 2rem;'>Welcome to our Startup!</h1>
+            <p style='text-align: center; font-size: 2rem; margin-bottom: 2rem;'>We are a team of specialists who can help you with anything you need.</p>
+            <p style='text-align: center; font-size: 2rem; margin-bottom: 2rem;'>Our goal is to provide you with the best possible service and make sure you are satisfied with our work.</p>
+            <div style='text-align: center;'>
+                <a href='/contact' style='background-color: #0072c6; color: white; font-size: 2rem; padding: 1rem 2rem; border-radius: 1rem; text-decoration: none;'>Contact Us</a>
+            </div>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 # About page
 def about():
@@ -20,26 +47,28 @@ def contact():
     st.write("- Phone: 555-1234")
     
 def app():
-    st.title("Welcome to our Startup!")
-    st.write("We are a team of specialists who can help you with anything you need.")
-    st.write("Our goal is to provide you with the best possible service and make sure you are satisfied with our work.")
-    st.write("Please click the button below to get in touch with us and learn more about our services.")
-    if st.button("Contact Us"):
-        st.write("You can reach us at info@startup.com")
+    # Define the contact page
+    st.title("Contact Us")
+    st.write("Please fill out the form below or ask us a question using the chatbot.")
+    
+    # Add a form for users to submit their contact information
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    message = st.text_area("Message")
+    submit = st.button("Submit")
+    
+    # Add the GPT-3 chatbot
+    st.sidebar.title("Chat with a Specialist")
+    prompt = st.sidebar.text_input("Ask a question")
+    if prompt:
+        response = generate_response(prompt)
+        st.sidebar.write(response)
+    
+    # Add a confirmation message after the form is submitted
+    if submit:
+        st.write("Thank you for contacting us! We will get back to you soon.")
  
-    st.markdown(
-        """
-        <div style='background-color: #f5f5f5; padding: 5rem 0;'>
-            <h1 style='text-align: center; font-size: 4rem; margin-bottom: 2rem;'>Welcome to our Startup!</h1>
-            <p style='text-align: center; font-size: 2rem; margin-bottom: 2rem;'>We are a team of specialists who can help you with anything you need.</p>
-            <p style='text-align: center; font-size: 2rem; margin-bottom: 2rem;'>Our goal is to provide you with the best possible service and make sure you are satisfied with our work.</p>
-            <div style='text-align: center;'>
-                <a href='/contact' style='background-color: #0072c6; color: white; font-size: 2rem; padding: 1rem 2rem; border-radius: 1rem; text-decoration: none;'>Contact Us</a>
-            </div>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+   
 if __name__ == '__main__':
     app()
 
