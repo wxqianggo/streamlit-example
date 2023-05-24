c03ce008a821e435e0e8c0b23e595cdd521d7e41
wxqianggo <wxqiang@gmail.com>
2023-05-24 14:59:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ openai.api_key = "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"
 # Define the function to generate responses using GPT-3
 def generate_response(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="davinci",
         prompt=prompt,
         max_tokens=1024,
         n=1,
